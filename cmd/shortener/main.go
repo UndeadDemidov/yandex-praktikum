3ce037ff3bc54ac7f51427f0324a2f4a110c8ada
nikolaydemidov <nikolay.a.demidov@yandex.ru>
2022-05-10 12:53:58 +0300

    7th increment is done

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -9,33 +9,24 @@ import (
 	"syscall"
 	"time"
 
-	"github.com/caarlos0/env/v6"
+	"github.com/spf13/viper"
 
 	"github.com/UndeadDemidov/yandex-praktikum/internal/app/handlers"
 	"github.com/UndeadDemidov/yandex-praktikum/internal/app/server"
 	"github.com/UndeadDemidov/yandex-praktikum/internal/app/storages"
 )
 
-type Config struct {
-	BaseURL         string `env:"BASE_URL" envDefault:"http://localhost:8080/"`
-	ServerAddress   string `env:"SERVER_ADDRESS" envDefault:":8080"`
-	FileStoragePath string `env:"FILE_STORAGE_PATH"`
-}
-
 func main() {
-	var cfg Config
-	err := env.Parse(&cfg)
-	if err != nil {
-		log.Fatal(err)
-	}
-
 	var s *http.Server
 	var r handlers.Repository
-	if r, err = storages.NewFileStorage(cfg.FileStoragePath); err == nil {
+	var err error
+
+	if r, err = storages.NewFileStorage(viper.GetString("file-storage-path")); err == nil {
 	} else {
 		r = storages.NewLinkStorage()
+		log.Print("In memory storage will be used")
 	}
-	s = server.NewServer(cfg.BaseURL, cfg.ServerAddress, r)
+	s = server.NewServer(viper.GetString("base-url"), viper.GetString("server-address"), r)
 
 	// Пришлось сделать graceful shutdown, чтобы правильно закрывать файлы при остановке сервера
 	done := make(chan os.Signal, 1)
@@ -46,10 +37,10 @@ func main() {
 			log.Fatalf("listen: %s\n", err)
 		}
 	}()
-	log.Print("Server Started")
+	log.Print("Server started")
 
 	<-done
-	log.Print("Server Stopped")
+	log.Print("Server stopped")
 
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer func() {
@@ -60,5 +51,5 @@ func main() {
 	if err := s.Shutdown(ctx); err != nil {
 		log.Fatalf("Server Shutdown Failed:%+v", err)
 	}
-	log.Print("Server Exited Properly")
+	log.Print("Server exited properly")
 }
